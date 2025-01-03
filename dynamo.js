ec552032df80c2731da493ebe185426dbfe6a427
Johno <john@mount-media-online.com>
2025-01-03 20:37:08 +0000

    aws setup

--- dynamo.js
+++ dynamo.js
@@ -1,2 +1,12 @@
 const AWS = require('aws-sdk');
 require('dotenv').config(); 
+
+
+AWS.config.update({
+    region:process.env.AWS_DEFAULT_REGION,
+    accessKeyId: process.env.AWS_ACESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = "company-api"
